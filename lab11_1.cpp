48b9eaf026a5e94562fc023ec75bc7c78390eb00
KoMoRee <atippoonkatevit@gmail.com>
2023-12-22 17:05:51 +0700

    All Finished.

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    int randg = rand() % 9;
+    string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    string og = grade[randg];
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i = 0 ; i < 3 ; ++i) cin.get();
+    
+    cout << "You will get " << og << " in this 261102.";
+    return 0;
+}
